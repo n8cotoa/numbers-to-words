2904183d64f6a3ca8a028884165bc278b9ca902a
Nate Cottle and Will Swanson <will@>
2018-06-26 10:58:27 -0700

    add more passing tests.

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,16 +1,27 @@
 def numbers_to_words(number)
   num_array = number.to_s.split("")
   num_array_length = num_array.length
-  ones_value = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
-  teens_value = {1 => "eleven", 2 => "twelve", 3 => "thirteen", 4 => "fourteen", 5 => "fifteen", 6 => "sixteen", 7 => "seventeen", 8 => "eighteen", 9 => "nineteen"}
-  tens_value = {1 => "ten", 2 => "twenty", 3 => "thirty", 4 => "fourty", 5 => "fifty", 6 => "sixty", 7 => "seventy", 8 => "eighty", 9 => "ninety"}
+  if num_array_length == 1
+    2.times do (num_array.unshift(0)) end
+  elsif num_array_length == 2
+    num_array.unshift(0)
+  end
+  ones_value = {0 => "", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
+  teens_value = {0 => "ten", 1 => "eleven", 2 => "twelve", 3 => "thirteen", 4 => "fourteen", 5 => "fifteen", 6 => "sixteen", 7 => "seventeen", 8 => "eighteen", 9 => "nineteen"}
+  tens_value = {2 => "twenty", 3 => "thirty", 4 => "fourty", 5 => "fifty", 6 => "sixty", 7 => "seventy", 8 => "eighty", 9 => "ninety"}
   word_output = ""
   if num_array[1] == "1"
-    word_output += (ones_value[num_array[0].to_i] + " hundred ")
+    if num_array[0] != 0
+      word_output += (ones_value[num_array[0].to_i] + " hundred ")
+    end
     word_output += (teens_value[num_array[2].to_i])
   else
-    word_output += (ones_value[num_array[0].to_i] + " hundred ")
+    if num_array[0] != 0
+      word_output += (ones_value[num_array[0].to_i] + " hundred ")
+    end
+    if num_array[1] != 0
     word_output += (tens_value[num_array[1].to_i] + " ")
+    end
     word_output += (ones_value[num_array[2].to_i])
   end
   word_output
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -2,13 +2,19 @@ require('rspec')
 require('numbers_to_words')
 
 describe('#numbers_to_words') do
-  # it('accept a number between 1 and 9 and converts it to word') do
-  #   expect(numbers_to_words(9)).to(eq("nine"))
-  # end
-  it('accept a number between 10 and 99 and converts it to word') do
+  it('accept a number between 1 and 9 and converts it to word') do
+    expect(numbers_to_words(9)).to(eq("nine"))
+  end
+  it('accept a number between 20 and 99 and converts it to word') do
+    expect(numbers_to_words(24)).to(eq("twenty four"))
+  end
+  it('accept a number between 10 and 19 and converts it to word') do
+    expect(numbers_to_words(11)).to(eq("eleven"))
+  end
+  it('accept a number between 100 and 999 and converts it to word') do
     expect(numbers_to_words(742)).to(eq("seven hundred fourty two"))
   end
-  it('accept a number between 10 and 99 which has a teen in it and converts it to word') do
+  it('accept a number between 100 and 999 which has a teen in it and converts it to word') do
     expect(numbers_to_words(312)).to(eq("three hundred twelve"))
   end
 end
